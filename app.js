d865362c940972e5bf9fb5ce7a86f94aaa8ab1ed
moses <mosesobike@gmail.com>
2021-02-08 19:39:01 +0100

    add colors

--- app.js
+++ app.js
@@ -13,6 +13,15 @@ let count = 0;
          }else if(styles.contains("reset")){
              count = 0
          }
+         if(count > 0){
+             value.style.color = "green"
+         }
+         if(count < 0){
+             value.style.color = "red"
+         }
+         if (count === 0){
+             value.style.color = "#222"
+         }
         value.textContent = count;
 
      });
